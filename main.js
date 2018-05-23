747e9e9bf7fbf9fcfbb66b583ea749c5d39c845d
andr3w <liu.anray+github@gmail.com>
2018-05-22 22:13:59 -0400

    Delete buttons

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-const plaintext = 'hello, world';
-const encryptedData = sjcl.encrypt('mypassword', plaintext);
-console.log('encryptedData:', encryptedData);
-const recoveredData = sjcl.decrypt('mypassword', encryptedData);
-console.log('recoveredData:', recoveredData);
-
 const mimibox = {
   entries: [
-    { name: 'Facebook', username: 'lookatme137', password: 'fb-password' },
-    { name: 'Amazon', username: 'shopper923', password: 'amazon-password' }
+    { id: '1342423423423', name: 'Facebook', username: 'lookatme137', password: 'fb-password' },
+    { id: '3565634353453', name: 'Amazon', username: 'shopper923', password: 'amazon-password' }
   ],
 };
