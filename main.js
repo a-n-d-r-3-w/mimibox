0a218054db24db0d96dcac1388aeda72c511106e
andr3w <liu.anray+github@gmail.com>
2018-05-22 22:47:31 -0400

    Consolidate code into index.html

--- main.js
+++
@@ -1,6 +0,0 @@
-const mimibox = {
-  entries: [
-    { id: '1342423423423', name: 'Facebook', username: 'lookatme137', password: 'fb-password' },
-    { id: '3565634353453', name: 'Amazon', username: 'shopper923', password: 'amazon-password' }
-  ],
-};
